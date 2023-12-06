d0bc2f854693e9b2e18df2a844504d99277f4cb6
Kacper <kapelek82@outlook.com>
2023-12-06 14:39:16 +0000

    Added UseEffect and fetch statements

--- src/containers/CatContainer.js
+++ src/containers/CatContainer.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 const CatContainer = () => {
+    
+    const [cat, setCat] = useState([]);
+
+    const fetchCat = async (name) => {
+        const response = await fetch(`https://cataas.com/cat ${name}`);
+        const catData = response.json();
+        setCat(catData);
+    }
+
+    useEffect(() => {
+        fetchCat();
+    }, [])
+
     return (
         <>
             <h2>This is the Cat Container</h2>
+            <Cat/>
         </>
     );
 }
