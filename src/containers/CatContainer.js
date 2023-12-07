87a6017061bee9c00ef899448c82ac4c6668cf5b
Piraven Nandakumaran <Xothspath@gmail.com>
2023-12-07 09:38:10 +0000

    Added functionality to allow user to choose to display a cat gif or cat image

--- src/containers/CatContainer.js
+++ src/containers/CatContainer.js
@@ -5,30 +5,43 @@ import CatImage from "../components/CatImage";
 const CatContainer = () => {
     
     const [cat, setCat] = useState(null);
+    const[catType,setCatType] = useState("/cat")
 
     const fetchCat = async () => {
-        const response = await fetch(`https://cataas.com/cat`, {
+        const response = await fetch(`https://cataas.com${catType}`, {
                 method: "GET",
                 headers: {"Content-Type": "application/json"}
           });
-        console.log(response)
         const catData = await response.blob();
         console.log(URL.createObjectURL(catData));
         setCat(URL.createObjectURL(catData));
     }
 
+    const handleCatTypeChange = (event) => {
+        setCatType(event.target.value);
+    }; 
+
     useEffect(() => {
         fetchCat();
-    }, [])
+    }, [catType])
 
-    
 
     return (
         <>
             <h2>This is the Cat Container</h2>
+            <nav>
+                 <select value={catType} onChange={handleCatTypeChange}>
+                    <option>Select an cat type</option>
+                    <option value="/cat">Cat image</option>
+                    <option value="/cat/gif">Cat Gif</option>
+                </select>
+
+                <CatButton onButtonClick = {fetchCat}/>
+            </nav>
             {cat ? <CatImage cat = {cat}/> : <p>Loading cat...</p>}
-            <CatButton onButtonClick = {fetchCat}/>
-            {/* <Cat cat = {cat}/> */}
+            
+            
+            
         </>
     );
 }
