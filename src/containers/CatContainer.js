348f0b3aa3af6bfb7c051e750a9188ac44244d8d
Kacper <kapelek82@outlook.com>
2023-12-07 15:56:59 +0000

    Added CSS and made design changes

--- src/containers/CatContainer.js
+++ src/containers/CatContainer.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import CatButton from "../components/CatButton";
 import CatImage from "../components/CatImage";
 import CatForm from "../components/CatForm";
+import '../index.css'
+
 
 const CatContainer = () => {
     
@@ -39,22 +41,25 @@ const CatContainer = () => {
 
 
     return (
+        <>
+        <style>
+            @import url('https://fonts.cdnfonts.com/css/chic-cat');
+        </style>       
         <div className="Everything-box">
-            <h2>This is the Cat Container</h2>
-            
+            <h2>Welcome to Cat-Utopia</h2>
                 <nav>
                     <select value={catType} onChange={handleCatTypeChange}>
                         <option>Select a cat type</option>
-                        <option value="/cat">Cat image</option>
+                        <option value="/cat">Cat Image</option>
                         <option value="/cat/gif">Cat Gif</option>
                         <option value="/cat/says">Cat Text</option>
                     </select>
                     {showForm && <CatForm handleFormChange = {handleFormChange}/>}
                     <CatButton onButtonClick = {fetchCat}/>
                 </nav>
-            
-            {cat ? <CatImage cat = {cat}/> : <p>Loading cat...</p>}    
         </div>
+        {cat ? <CatImage cat = {cat}/> : <p>Loading cat...</p>}  
+        </>
     );
 }
  
